2755e9180f57928ee0759ffe15b5e60178273ed2
Pavel_Vatsek <pvatsek@gmail.com>
2023-06-19 23:38:05 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        final RedBlackTree tree = new RedBlackTree();
+        tree.add(2);
+        tree.add(5);
+        tree.add(7);
+        tree.add(2);
+        tree.add(9);
+        tree.add(12);
+        tree.add(1);
+        tree.add(0);
+        tree.add(76);
+        tree.add(45);
+        tree.add(54);
+    }
+}
